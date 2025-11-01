a67c1045e4655f28b81ac4b6e171743a13eb7760
Sagar Mitra <mitrasagar10@gmail.com>
2025-11-02 00:43:54 +0530

    update: router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
+import TVShows from "./pages/TVShows";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/tv-shows", element: <TVShows /> },
+    ],
   },
 ]);
 
