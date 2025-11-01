54b2dccd0aea6be7833627e05b7fcc27d4ca98b1
Sagar Mitra <mitrasagar10@gmail.com>
2025-11-02 01:30:23 +0530

    update: add social meadia links in constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -17,3 +17,9 @@ export const NAV_LINKS = [
 export const PROFILE_PIC_URL = "https://i.pinimg.com/736x/9a/5f/f0/9a5ff060152ceb9b8c4b5596cf81878c.jpg"
 
 export const IMAGE_URL = "https://image.tmdb.org/t/p/original/";
+
+export const GITHUB_URL = "https://github.com/devxsagar/Moive-Matrix";
+
+export const LINKEDIN_URL = "https://www.linkedin.com/in/sagar-mitra19/";
+
+export const TWITTER_URL = "https://x.com/devxsagar";
