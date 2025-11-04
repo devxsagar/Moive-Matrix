6d1d734ff99288467a6d694656cab4c1120768db
Sagar Mitra <mitrasagar10@gmail.com>
2025-11-04 20:32:38 +0530

    refactor: update useMediaCredits

--- src/hooks/useMediaCredits.js
+++ src/hooks/useMediaCredits.js
@@ -1,25 +1,46 @@
 import { useEffect, useState } from "react";
 import { API_OPTIONS } from "@/utils/constant";
 
-const useMediaCredits = ( mediaType, id ) => {
+const useMediaCredits = (mediaType, id) => {
   const [directors, setDirectors] = useState({});
   const [cast, setCast] = useState({});
-
-  const getCredits = async () => {
-    const url = `https://api.themoviedb.org/3/${mediaType}/${id}/credits?language=en-US`;
-
-    const res = await fetch(url, API_OPTIONS);
-    const json = await res.json();
-
-    setCast(json.cast);
-    setDirectors(json.crew.filter((member) => member.known_for_department === "Directing"));
-  };
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getCredits();
-  }, []);
+    if (!mediaType || !id) return;
 
-  return { directors, cast };
+    const controller = new AbortController();
+
+    const getCredits = async () => {
+      try {
+        setLoading(true);
+
+        const url = `https://api.themoviedb.org/3/${mediaType}/${id}/credits?language=en-US`;
+
+        const res = await fetch(url, API_OPTIONS);
+        const json = await res.json();
+
+        setCast(json.cast);
+        setDirectors(json.crew.filter((member) => member.known_for_department === "Directing"));
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.error("Error fetching similar media:", err);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getCredits();
+
+    return () => {
+      controller.abort();
+    };
+  }, [mediaType, id]);
+
+
+
+  return { directors, cast, loading };
 };
 
 export default useMediaCredits;
